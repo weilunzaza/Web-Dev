3261c07bd3e6dbaf4101b0f534e65807762908a1
MarcoEditia <105383802+MarcoEditia@users.noreply.github.com>
2024-05-23 16:12:07 +0800

    Update java.js
    mobile responsive navbar javascript

--- java.js
+++ java.js
@@ -1,1 +1,18 @@
+function nav() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const show = document.querySelector('.nav__burger');
+        const navMenu = document.querySelector('.nav__menu');
+        const hide = document.querySelector('.fa-times');
 
+        show.addEventListener('click', () => {
+                navMenu.classList.add('active');
+                //Animate Menu
+            });
+
+        hide.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+        });
+    });
+}
+
+nav();
