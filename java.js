a8cf2a2f9fbd2574b93f36e4f9cc0bb81ec30977
wei lun <chinweilun2000@gmail.com>
2024-06-16 21:56:06 +0800

    new homepage

--- java.js
+++ java.js
@@ -39,3 +39,11 @@ function observer(){
 observer()
 });
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
