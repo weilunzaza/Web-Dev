701e3aa0110ea7b58b53e2331e487352ad49b3ec
wei lun <chinweilun2000@gmail.com>
2024-06-19 15:49:26 +0800

    homepageupdates

--- homepage.js
+++ homepage.js
@@ -1,7 +1,50 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+document.addEventListener('DOMContentLoaded', () => {
+  /*function nav() {
+      
+          const show = document.querySelector('.nav__burger');
+          const navMenu = document.querySelector('.nav__menu');
+          const hide = document.querySelector('.fa-times');
+  
+          show.addEventListener('click', () => {
+                  navMenu.classList.add('active');
+                  //Animate Menu
+              });
+  
+          hide.addEventListener('click', () => {
+              navMenu.classList.remove('active');
+          });
+  
   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  nav(); */
+  
+  function observer(){
+      const cards = document.querySelectorAll('.HomepageInfo__card')
+      const updateHeader = document.querySelector('.HomepageUpdates__header')
+      const appear =  new IntersectionObserver(
+          observedItems => {
+              observedItems.forEach(observedItem =>{
+                  observedItem.target.classList.toggle("appear", observedItem.isIntersecting)
+                  if (observedItem.isIntersecting) appear.unobserve(observedItem.target)
+              });
+          },
+          {threshold:1}); 
+      cards.forEach(card => {
+          appear.observe(card)
+      });
+  
+      appear.observe(updateHeader)
   }
+  
+  observer()
+  });
+  
+  function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+  
+  
